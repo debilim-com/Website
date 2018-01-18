6fba1676219d9e30a8e34ede409683a89f9762d6
Ali Asadpoor <aasadpoor@debilim.com>
2018-01-18 05:06:45 -0600

    Add items to first page.

--- www/html/index.php
+++ www/html/index.php
@@ -1,5 +1,55 @@
-<?php
-
-echo "Hello World!";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>DeBilim</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <a href="./" style="text-decoration: none;">
+                    <img style="width: 5em;height: 5em;" src="Resource/Logo.png" />
+                </a>
+            </td>
+            <td>
+                English
+            </td>
+            <td>
+                Persain
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <img style="width: 20em;height: 10em;" src="Resource/General.png" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                You will get all you want in life, if you help enough other people get what they want.
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>
+                zig ziglar
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                You are whitin first 100 visitors and you will get a free service from DeBilim. <br />
+                Some time later whitin first 1000.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input id="id_guest_email" type="email" />
+            </td>
+            <td>
+                <input type="submit" value="Notify Me" />
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
